a6b5200065f7556fef39a2ec096d831dd02458ba
Adan <adanlohgit@gmail.com>
2026-03-11 14:00:16 -0600

    Segundo avance con registro

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,9 +1,13 @@
 # usuario/urls.py
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     # Usamos la vista mágica de Django, solo le decimos qué HTML usar
     path('login/', auth_views.LoginView.as_view(template_name='usuario/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('registro/', views.registro, name='registro'), 
+    path('registro/profesor/', views.registro_profesor, name='registro_profesor'),
+    path('registro/alumno/', views.registro_alumno, name='registro_alumno'),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
+def registro(request):
+    # Si el usuario le dio clic al botón de "Crear Cuenta" (Método POST)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save() # ¡Aquí ocurre la magia! Se guarda en la base de datos
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Cuenta creada para {username}. Ya puedes iniciar sesión.')
+            return redirect('login') # Lo mandamos a que inicie sesión
+    
+    # Si el usuario apenas entró a la página a ver el formulario (Método GET)
+    else:
+        form = UserCreationForm()
+        
+    # Le mandamos el formulario vacío a tu HTML
+    return render(request, 'usuario/registro.html', {'form': form})
 # Create your views here.
+def registro_profesor(request):
+    return render(request, 'usuario/registro_profesor.html')
+
+def registro_alumno(request):
+    return render(request, 'usuario/registro_alumno.html')
